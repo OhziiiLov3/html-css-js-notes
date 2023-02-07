fa4c828ce8ca637eb4d16912bb06314c72f798a0
ohziiilov3 <klbaskerville0520@gmail.com>
2023-02-07 08:54:56 -0800

    chore: add html, css, js files, and added noted to js files

---
+++ main.js
@@ -0,0 +1,43 @@
+                    /* Intro to JavaScript */
+
+console.log("Hello World")
+
+let myName = "Keith"
+const anotherName = "Ohz"
+console.log(myName)
+console.log(anotherName)
+
+//  const and let 
+//  let means it can be updated 
+//  const means is better not be 
+
+let someVariable;
+someVariable = "5";
+someVariable += 3;
+someVariable -= 2;
+
+// prompt is a bult in function 
+// let userName = prompt("Enter your username")
+// someVariable = userName.length
+// console.log(userName);
+console.log(typeof someVariable);
+console.log(someVariable);
+
+// Adding Strings together = String concatenation
+let firstName = "Zooty"
+let LastName = "World"
+let fullName = firstName + " " + LastName
+console.log(fullName);
+
+
+
+
+
+
+
+
+
+
+
+
+
