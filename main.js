664d448b337f99ac8a9f3384fc4a9f295b8cf135
ohziiilov3 <klbaskerville0520@gmail.com>
2023-02-07 10:42:32 -0800

    chore: add datatypes

--- main.js
+++ main.js
@@ -29,8 +29,36 @@ let LastName = "World"
 let fullName = firstName + " " + LastName
 console.log(fullName);
 
+// Datatypes 
+// Booleans - is this true or false 
+someCode = true
+if(someCode){
+    // run this code 
+
+}else{
+    // run this code 
+}
+
+// Objects 
+let objectExmaple = {
+    key: "value"
+}
+// Arrays 
+let thisItem = [1,2,3,45,89,[10,11]]
+thisItem.forEach((item,idx) => console.log(`value is item ${item} and the index is ${idx}`));
+
+// let thisItem2 = [ 1,"something", {a:"b"}, [4,5],7]
+// for(let i = 0; i < thisItem2.length; i++){
+//     console.log(`The value is ${thisItem2[i]} and the index is ${i}`)
+// }
 
 
+// if("+" && typeof item1 === "string" && typeof item2 === 'number') {
+//     item2.toString()
+// } else if("-" && typeof item1 === "string" && typeof item2 === 'number') {
+//     // parseInt(item1);
+//     // parseFloat(item1).toFixed(2);
+// }
 
 
 
